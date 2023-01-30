3e845a8d17fc834a42c4ca9beb3719357cda3fb9
Iyappan Subramanian <isubramanian@amamperecomputing.com>
2023-01-30 21:43:19 +0000

    Updated docker tag

--- start-yolo-demo.sh
+++ start-yolo-demo.sh
@@ -5,12 +5,12 @@ export FLASK_RUN_PORT=5001
 export NUM_THREADS=32
 
 # Select one of the following
-#export VIDEO_SRC='examples/Driving-NYC-360p.mp4'
-export VIDEO_SRC='examples/Driving-Vegas-360p.mp4'
-#export VIDEO_SRC='examples/3-640x360-600k.mp4'
+#export VIDEO_SRC='examples/Driving-NYC-360p-jutah.mp4'
+export VIDEO_SRC='examples/Driving-Vegas-360p-jutah.mp4'
+#export VIDEO_SRC='examples/Driving-Medley-jutah.mp4'
 #export VIDEO_SRC='examples/video-small-640.mp4'
 #export VIDEO_SRC='examples/video-test.mp4'
-#export VIDEO_SRC='examples/ParisMorningRain.mp4'
+#export VIDEO_SRC='examples/ParisMorningRain-jutah.mp4'
 #export VIDEO_SRC='https://www.youtube.com/watch?v=1EiC9bvVGnk'
 
 docker compose up yolo-demo
