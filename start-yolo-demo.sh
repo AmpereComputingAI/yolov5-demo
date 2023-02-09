f91450ae53af349becb09c166a835879a3451ceb
Iyappan Subramanian <isubramanian@amperecomputing.com>
2023-02-09 23:19:58 +0000

    Code cleanup

--- start-yolo-demo.sh
+++ start-yolo-demo.sh
@@ -9,8 +9,7 @@ export VIDEO_SRC='examples/Driving-NYC-360p-jutah.mp4'
 #export VIDEO_SRC='examples/Driving-Vegas-360p-jutah.mp4'
 #export VIDEO_SRC='examples/Driving-Medley-jutah.mp4'
 #export VIDEO_SRC='examples/video-small-640.mp4'
-#export VIDEO_SRC='examples/video-test.mp4'
 #export VIDEO_SRC='examples/ParisMorningRain-jutah.mp4'
 #export VIDEO_SRC='https://www.youtube.com/watch?v=1EiC9bvVGnk'
 
-docker compose up yolo-demo
+docker compose up app-yolo-live
