171103acc361273911efa19d83d9feb6d2520f5b
Iyappan Subramanian <isubramanian@amperecomputing.com>
2023-02-23 19:44:42 +0000

    Updated docker tag and scripts
    - Updated docker tag
    - Added port mapping
    - Updated cpu and gpu start, stop scripts

--- start-yolo-cpu.sh
+++ start-yolo-cpu.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-export FLASK_RUN_HOST="0.0.0.0"
-export FLASK_RUN_PORT=5001
-export NUM_THREADS=32
-
 # Select one of the following
-#export VIDEO_SRC=0
-export VIDEO_SRC='examples/Driving-NYC-360p-jutah.mp4'
+#export VIDEO_SRC='examples/Driving-NYC-360p-jutah.mp4'
 #export VIDEO_SRC='examples/Driving-Vegas-360p-jutah.mp4'
 #export VIDEO_SRC='examples/Driving-Medley-jutah.mp4'
 #export VIDEO_SRC='examples/video-small-640.mp4'
 #export VIDEO_SRC='examples/ParisMorningRain-jutah.mp4'
 #export VIDEO_SRC='https://www.youtube.com/watch?v=1EiC9bvVGnk'
 
-docker compose up app-yolo-live
+export FLASK_RUN_HOST="0.0.0.0"
+export FLASK_RUN_PORT=5000
+export NUM_THREADS=32
+export HOST_PORT=5000
+export VIDEO_SRC='examples/Driving-NYC-360p-jutah.mp4'
+
+cont_name="cont-1"
+docker compose -p $cont_name up -d app-yolo-live
---
+++ start-yolo-gpu-4.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Select one of the following
+#export VIDEO_SRC='examples/Driving-NYC-360p-jutah.mp4'
+#export VIDEO_SRC='examples/Driving-Vegas-360p-jutah.mp4'
+#export VIDEO_SRC='examples/Driving-Medley-jutah.mp4'
+#export VIDEO_SRC='examples/video-small-640.mp4'
+#export VIDEO_SRC='examples/ParisMorningRain-jutah.mp4'
+#export VIDEO_SRC='https://www.youtube.com/watch?v=1EiC9bvVGnk'
+
+export CUDA_VISIBLE_DEVICES='0'
+export FLASK_RUN_HOST="0.0.0.0"
+export FLASK_RUN_PORT=5001
+export NUM_THREADS=32
+export HOST_PORT=5000
+export VIDEO_SRC='examples/Driving-NYC-360p-jutah.mp4'
+
+cont_name="cont-gpu-1"
+docker compose -p $cont_name up -d app-yolo-live-gpu
+
+export HOST_PORT=5001
+export VIDEO_SRC='examples/Driving-Vegas-360p-jutah.mp4'
+cont_name="cont-gpu-2"
+docker compose -p $cont_name up -d app-yolo-live-gpu
+
+export HOST_PORT=5002
+export VIDEO_SRC='examples/Driving-Medley-jutah.mp4'
+cont_name="cont-gpu-3"
+docker compose -p $cont_name up -d app-yolo-live-gpu
+
+export HOST_PORT=5003
+export VIDEO_SRC='examples/ParisMorningRain-jutah.mp4'
+cont_name="cont-gpu-4"
+docker compose -p $cont_name up -d app-yolo-live-gpu
